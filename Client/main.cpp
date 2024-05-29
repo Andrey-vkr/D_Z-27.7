7eb34638b2fe4b5bb78da4e54fa2192b67d2ead3
Andrey <and.vkr@mail.ru>
2024-05-30 01:01:14 +0300

    1_file

---
+++ Client/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <string>
+
+#include "Client.h"
+#include "Chat.h"
+
+
+int main()
+{
+	std::string clientMessage, serverMessage;
+
+	Client client{};
+	Chat chat{};
+
+	client.createSocket();
+	client.connectionToServer();
+
+	chat.startMessage();
+
+	while (1)
+	{
+		clientMessage = chat.chatProcessing();
+		client.writeData(clientMessage);
+		if (clientMessage == "exit")
+		{
+			break;
+		}
+		serverMessage = client.readData();
+		chat.messageProcessing(serverMessage);
+	}
+
+	client.closeSocket();
+
+	return 0;
+}
